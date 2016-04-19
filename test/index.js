5f6477ce11b491020a8c35eeac652daa3c8270b0
Yamil Asusta <yamil@rebelmail.com>
2016-04-19 16:17:44 -0400

    Match auth conf of other transports

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ const assert = require('assert');
 describe('ResponsysTransport', () => {
   describe('#login', () => {
     it('should return with stored token', (done) => {
-      const transporter = responsys({ token: 1 });
+      const transporter = responsys({ token: 1 auth: {} });
       transporter.login((err, token) => {
         assert.ifError(err);
         assert.ok(token);
@@ -16,14 +16,14 @@ describe('ResponsysTransport', () => {
       });
     });
     it('should return if there is an error', (done) => {
-      const transporter = responsys({ username: 'test', password: '123' });
+      const transporter = responsys({ auth: { username: 'test', password: '123' } });
       transporter.login((err) => {
         assert.ok(err);
         done();
       });
     });
     it('should return token if successful', (done) => {
-      const transporter = responsys({ username: 'test', password: '123' });
+      const transporter = responsys({ auth: { username: 'test', password: '123' } });
       const server = nock(transporter.rootURL).post('/rest/api/v1.1/auth/token').reply(200, {
         authToken: 123
       });
@@ -36,14 +36,14 @@ describe('ResponsysTransport', () => {
   });
   describe('#send', () => {
     it('should return error from #login if failed', (done) => {
-      const transporter = responsys({ username: 'test', password: '123' });
+      const transporter = responsys({ auth: { username: 'test', password: '123' } });
       transporter.login((err, token) => {
         assert.ok(err);
         done();
       });
     });
     it('should return the results of the call', (done) => {
-      const transporter = responsys({ token: 1, campaignName: 'test' });
+      const transporter = responsys({ token: 1, campaignName: 'test', auth: {} });
       const server = nock(transporter.rootURL).post('/rest/api/v1/campaigns/test/email').reply(200);
       transporter.send({ data: { to: 'test@test.com' }} , (err) => {
         assert.ifError(err);
